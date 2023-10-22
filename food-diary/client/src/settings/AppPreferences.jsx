88693fe7e5f3279d996419e9ccba3fdad086c12f
Solana Ayanle <solana.ayanle@gmail.com>
2023-10-22 20:38:38 +0200

    Add components

---
+++ food-diary/client/src/settings/AppPreferences.jsx
@@ -0,0 +1,3 @@
+// Language Selection: If your app supports multiple languages, users can select their preferred language.
+// Date and Time Format: Give users the flexibility to change how dates and times are displayed.
+// Measurement Units: Let users choose between metric and imperial units for height, weight, and other measurements.
---
+++ food-diary/client/src/settings/ProfileSettings.jsx
@@ -0,0 +1,2 @@
+// Profile Information: Allow users to update their name, email, profile picture, and other personal details.
+// Change Password: Provide a form to change the user's password.
